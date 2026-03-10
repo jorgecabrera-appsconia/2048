f763d00fa2a5b47cb41fb0f2c592f821e0b92d09
Jorge Antonio Cabrera <cabrerajorgea@gmail.com>
2026-03-09 21:09:03 -0300

    Ahora es un PWA

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = '2048-verduras-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './icon.svg',
+    './manifest.json',
+    'https://fonts.googleapis.com/css2?family=Outfit:wght@400;600;800&display=swap',
+    'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
